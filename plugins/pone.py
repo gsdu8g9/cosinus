5aaa2a874fbde96adbbd5afebe5cc6961cbe9fd7
hdk5 <hdk5@users.noreply.github.com>
2016-02-23 23:14:31 +0300

    Пони. Потому что.

---
+++ plugins/pone.py
@@ -0,0 +1,39 @@
+import io
+import random
+
+import requests
+
+from bot import vkapi
+
+event_id = 4
+
+
+def call(event):
+    if event[6].partition(' ')[0] == '/pony':
+        search_req = 'score.gt:200,safe,-animated,-svg,-comic'
+        search_url = 'https://derpiboo.ru/search.json'
+        page_req = requests.get("{b}?q={s}".format(b=search_url, s=search_req), timeout=3)
+        page_req.raise_for_status()
+        total = page_req.json()['total']
+        pages = -(-total // 15)
+        page = random.randint(1, pages)
+        req = requests.get("{b}?q={s}&page={p}".format(b=search_url, s=search_req, p=page), timeout=3)
+        req.raise_for_status()
+        random_img = random.choice(req.json()['search'])
+        image_url = 'https:' + random_img['image']
+        image_dbid = random_img['id_number']
+        image_src = random_img['source_url']
+        image_req = requests.get(image_url)
+        image_req.raise_for_status()
+        image = io.BytesIO(image_req.content)
+
+        upload_server = vkapi.photos.getMessagesUploadServer()
+        upload_req = requests.post(upload_server['upload_url'],
+                                   files={'photo': ('image.png', image, image_req.headers['content-type'])})
+        upload_resp = vkapi.photos.saveMessagesPhoto(**upload_req.json())
+
+        image_vkid = upload_resp[0]['id']
+
+        vkapi.messages.send(message='http://derpibooru.org/' + str(image_dbid) + '\n' + image_src,
+                            attachment='photo348580470_' + str(image_vkid),
+                            peer_id=event[3])
