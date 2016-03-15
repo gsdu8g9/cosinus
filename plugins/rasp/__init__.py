5bb9c293217372a71fa7ed5a66251f6c44773f43
hdk5 <hdk5@users.noreply.github.com>
2016-03-15 20:27:44 +0300

    tz

--- plugins/rasp/__init__.py
+++ plugins/rasp/__init__.py
@@ -5,28 +5,36 @@ from .rasp_a import rasp_a
 from .rasp_fix import rasp_fix
 
 event_id = 4
-time_offset = datetime.timedelta(hours=8)
 
+class MskTime(datetime.tzinfo):
+    def tzname(self, dt):
+        return "Europe/Moscow"
+    def dst(self, dt):
+        return datetime.timedelta(0)
+    def utcoffset(self, dt):
+        return datetime.timedelta(hours=3)
+
+msk = MskTime()
 
 def call(event):
     if event[6].partition(' ')[0].lower() == '/пары':
-        current_time = datetime.datetime.now() + time_offset
-
+        # TODO: имена переменных
+        current_time = datetime.datetime.now(tz=msk)
         week_number = current_time.isocalendar()[1] % 2
         week_parity = 2 if week_number == 0 else 1
 
         current_lesson = None
         next_lesson = None
 
-        def get_lesson_time(st):
-            t = datetime.datetime.strptime(st, '%H:%M')
-            t = t.replace(year=current_time.year, month=current_time.month, day=current_time.day)
-            return t
+        # def get_lesson_time(st):
+        #     t = datetime.datetime.strptime(st, '%H:%M')
+        #     t = t.replace(year=current_time.year, month=current_time.month, day=current_time.day)
+        #     return t
 
-        def print_lesson(lesson):
+        def format_lesson(lesson):
             lesson_info = ['Предмет: ', lesson['name'], '\n',
-                           'Начало: ', lesson['start'], '\n',
-                           'Окончание: ', lesson['end'], '\n',
+                           'Начало: ', lesson['start'].strftime('%H:%M'), '\n',
+                           'Окончание: ', lesson['end'].strftime('%H:%M'), '\n',
                            'Аудитория: ', lesson['class'], '\n',
                            'Препод: ', lesson['teacher']]
             return ''.join(lesson_info)
@@ -38,11 +46,11 @@ def call(event):
 
         for lesson in today_rasp:
             if lesson['week'] == 0 or lesson['week'] == week_parity:
-                start = get_lesson_time(lesson['start'])
-                end = get_lesson_time(lesson['end'])
-                if (current_time > start) and (current_time < end):
+                start = lesson['start']
+                end = lesson['end']
+                if (current_time.time() > start) and (current_time.time() < end):
                     current_lesson = lesson
-                if current_time < start:
+                if current_time.time() < start:
                     next_lesson = lesson
                     break
         else:
@@ -66,13 +74,13 @@ def call(event):
 
         if current_lesson is not None:
             reply += ['Текущая пара:']
-            reply += [print_lesson(current_lesson)]
+            reply += [format_lesson(current_lesson)]
         else:
             reply += ['Сейчас пары нет']
 
         if next_lesson is not None:
             reply += ['Следующая пара:']
-            reply += [print_lesson(next_lesson)]
+            reply += [format_lesson(next_lesson)]
         else:
             reply += ['Что-то не так. Этот не должен быть достижим']
 
