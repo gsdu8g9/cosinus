feebd3b6b0ba4d265b76ffa8df3b7179b47efa71
hdk5 <hdk5@users.noreply.github.com>
2016-09-12 01:21:12 +0300

    Обновление расписания

--- plugins/rasp/rasp_a.py
+++ plugins/rasp/rasp_a.py
@@ -1,141 +1,137 @@
 import datetime
 
 rasp_a = [
-    [{"name": "Физ-ра",
+
+    # Понедельник
+    [],
+
+    # Вторник
+    [{"name": "Оптика и атомная физика (лабы)",
+      "week": 2,
+      "start": datetime.time(hour=9,minute=50),
+      "end": datetime.time(hour=11,minute=20),
+      "class": "кафедра Физики",
+      "teacher": "Посредник Олеся Валерьевна, Кузьмина Наталья Николаевна"},
+
+     {"name": "Алгоритмы и структуры данных (лекция)",
       "week": 0,
-      "start": datetime.time(hour=8,minute=0),
-      "end": datetime.time(hour=9,minute=30),
+      "start": datetime.time(hour=11,minute=40),
+      "end": datetime.time(hour=13,minute=10),
+      "class": "5427",
+      "teacher": "Фомичева Татьяна Генриховна"},
+
+     {"name": "Оптика и атомная физика (лекция)",
+      "week": 0,
+      "start": datetime.time(hour=13,minute=45),
+      "end": datetime.time(hour=15,minute=15),
+      "class": "5419",
+      "teacher": "Шейнман Илья Львович"},
+
+     {"name": "Физкультура",
+      "week": 0,
+      "start": datetime.time(hour=15,minute=35),
+      "end": datetime.time(hour=17,minute=5),
+      "class": "4 корпус",
+      "teacher": "-"}],
+
+    # Среда
+    [{"name": "Архитектура компьютера (практика)",
+      "week": 0,
+      "start": datetime.time(hour=11,minute=40),
+      "end": datetime.time(hour=13,minute=10),
+      "class": "3402",
+      "teacher": "Кирьянчиков Владимир Андреевич"},
+
+     {"name": "Алгоритмы и структуры данных (практика)",
+      "week": 0,
+      "start": datetime.time(hour=13,minute=45),
+      "end": datetime.time(hour=15,minute=15),
+      "class": "3413",
+      #TODO: имя препода
+      "teacher": "написать имя препода"},
+
+     {"name": "Экономика организации (практика)",
+      "week": 0,
+      "start": datetime.time(hour=15,minute=35),
+      "end": datetime.time(hour=17,minute=5),
+      "class": "4301",
+      #TODO: имя препода
+      "teacher": "написать имя препода"},
+
+     {"name": "Иностранный язык",
+      "week": 0,
+      "start": datetime.time(hour=17,minute=25),
+      "end": datetime.time(hour=18,minute=55),
+      "class": "кафедра иностранных языков",
+      "teacher": "-"}],
+
+    # Четверг
+    [{"name": "Специальные разделы математического анализа (практика)",
+      "week": 1,
+      "start": datetime.time(hour=9,minute=50),
+      "end": datetime.time(hour=11,minute=20),
+      "class": "2412",
+      "teacher": "Колпаков Андрей Сергеевич"},
+
+     {"name": "Физкультура",
+      "week": 0,
+      "start": datetime.time(hour=11,minute=40),
+      "end": datetime.time(hour=13,minute=10),
       "class": "4 корпус",
       "teacher": "-"},
 
-     {"name": "Матан (лекц)",
+     {"name": "Экономика организации (лекция)",
+      "week": 0,
+      "start": datetime.time(hour=13,minute=45),
+      "end": datetime.time(hour=15,minute=15),
+      "class": "5423",
+      "teacher": "Ширяева Татьяна Петровна"},
+
+     {"name": "Вычислительная математика (лекция)",
+      "week": 0,
+      "start": datetime.time(hour=15,minute=35),
+      "end": datetime.time(hour=17,minute=5),
+      "class": "3324",
+      "teacher": "Лисс Александр Рудольфович"}],
+
+    # Пятница
+    [{"name": "Архитектура компьютера (лекция)",
+      "week": 0,
+      "start": datetime.time(hour=11,minute=40),
+      "end": datetime.time(hour=13,minute=10),
+      "class": "3107",
+      "teacher": "Кирьянчиков Владимир Андреевич"},
+
+     {"name": "Специальные разделы математического анализа (лекция)",
+      "week": 0,
+      "start": datetime.time(hour=13,minute=45),
+      "end": datetime.time(hour=15,minute=15),
+      "class": "5419",
+      "teacher": "Солнышкин Сергей Николаевич"},
+
+     {"name": "Вычислительная математика (практика)",
+      "week": 0,
+      "start": datetime.time(hour=15,minute=35),
+      "end": datetime.time(hour=17,minute=5),
+      "class": "3413",
+      "teacher": "Щеголева Надежда Львовна"}],
+
+    # Суббота
+    [{"name": "Математическая логика и теория алгоритмов (практика)",
+      "week": 0,
+      "start": datetime.time(hour=8,minute=0),
+      "end": datetime.time(hour=9,minute=30),
+      "class": "3311",
+      "teacher": "Рыбин Сергей Витальевич"},
+
+     {"name": "Математическая логика и теория алгоритмов (лекция)",
       "week": 0,
       "start": datetime.time(hour=9,minute=50),
       "end": datetime.time(hour=11,minute=20),
       "class": "3308",
-      "teacher": "Солнышкин Сергей Николаевич"},
+      "teacher": "Рыбин Сергей Витальевич"}],
 
-     {"name": "Дискретная математика (пр)",
-      "week": 0,
-      "start": datetime.time(hour=11,minute=40),
-      "end": datetime.time(hour=13,minute=10),
-      "class": "2114",
-      "teacher": "Чухнов Антон Сергеевич"}],
-
-    [{"name": "Экономика (пр)",
-      "week": 2,
-      "start": datetime.time(hour=11,minute=40),
-      "end": datetime.time(hour=13,minute=10),
-      "class": "3426",
-      "teacher": "Заставенко Екатерина Валерьевна"},
-
-     {"name": "АиГ (лекц)",
-      "week": 0,
-      "start": datetime.time(hour=13,minute=45),
-      "end": datetime.time(hour=15,minute=15),
-      "class": "3308",
-      "teacher": "Зельвенский Игорь Григорьевич"},
-
-     {"name": "Матан (пр)",
-      "week": 0,
-      "start": datetime.time(hour=15,minute=35),
-      "end": datetime.time(hour=17,minute=5),
-      "class": "3313",
-      "teacher": "Коточигов Александр Михайлович"},
-
-     {"name": "АиГ (лекц)",
-      "week": 0,
-      "start": datetime.time(hour=17,minute=25),
-      "end": datetime.time(hour=18,minute=55),
-      "class": "3428",
-      "teacher": "Зельвенский Игорь Григорьевич"}],
-
-    [{"name": "Экономика (лекц)",
-      "week": 1,
-      "start": datetime.time(hour=8,minute=0),
-      "end": datetime.time(hour=9,minute=30),
-      "class": "5405",
-      "teacher": "Ягья Талие Саидовна"},
-
-     {"name": "Англ.яз.",
-      "week": 0,
-      "start": datetime.time(hour=9,minute=55),
-      "end": datetime.time(hour=11,minute=20),
-      "class": "каф. Ин.яз",
-      "teacher": "-"},
-
-     {"name": "Физика (лекц)",
-      "week": 0,
-      "start": datetime.time(hour=11,minute=40),
-      "end": datetime.time(hour=13,minute=10),
-      "class": "3107",
-      "teacher": "Шейнман Илья Львович"},
-
-     {"name": "Физ-ра",
-      "week": 0,
-      "start": datetime.time(hour=13,minute=45),
-      "end": datetime.time(hour=15,minute=15),
-      "class": "4 корпус",
-      "teacher": "-"}],
-
-    [{"name": "История (лекц)",
-      "week": 1,
-      "start": datetime.time(hour=13,minute=45),
-      "end": datetime.time(hour=15,minute=15),
-      "class": "5423",
-      "teacher": "Меньшиков Дмитрий Никитович"},
-
-     {"name": "Физ-ра",
-      "week": 2,
-      "start": datetime.time(hour=13,minute=45),
-      "end": datetime.time(hour=15,minute=15),
-      "class": "4 корпус",
-      "teacher": "-"},
-
-     {"name": "Программирование (лекц)",
-      "week": 0,
-      "start": datetime.time(hour=15,minute=35),
-      "end": datetime.time(hour=17,minute=5),
-      "class": "5405",
-      "teacher": "Самойленко Владимир Петрович"},
-
-     {"name": "Программирование",
-      "week": 0,
-      "start": datetime.time(hour=17,minute=25),
-      "end": datetime.time(hour=18,minute=55),
-      "class": "3402",
-      "teacher": "Самойленко Владимир Петрович"}],
-
-    [],
-
-    [{"name": "История",
-      "week": 0,
-      "start": datetime.time(hour=11,minute=40),
-      "end": datetime.time(hour=13,minute=10),
-      "class": "3417",
-      "teacher": "Стогов Дмитрий Игоревич"},
-
-     {"name": "Дискретная математика (лекц)",
-      "week": 0,
-      "start": datetime.time(hour=13,minute=45),
-      "end": datetime.time(hour=15,minute=15),
-      "class": "3308",
-      "teacher": "Рыбин Сергей Витальевич"},
-
-     {"name": "Физика (пр)",
-      "week": 1,
-      "start": datetime.time(hour=15,minute=35),
-      "end": datetime.time(hour=17,minute=5),
-      "class": "3107",
-      "teacher": "Морозов Вениамин Васильевич"},
-
-     {"name": "Физика (лаб)",
-      "week": 2,
-      "start": datetime.time(hour=15,minute=35),
-      "end": datetime.time(hour=17,minute=5),
-      "class": "каф. Физики",
-      "teacher": "Иманбаева Райхан Талгатовна, Демидов Юрий Андреевич"}],
-
+    # Воскресенье
     []
 
 ]
