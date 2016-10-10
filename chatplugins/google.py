bb717e534f8b1a78235707a00b9948f1a6e33d90
hdk5 <hdk5@users.noreply.github.com>
2016-10-10 14:51:57 +0300

    I'm feeling lucky для /google

--- chatplugins/google.py
+++ chatplugins/google.py
@@ -1,9 +1,13 @@
 from bot import AbstractChatPlugin
 
+import requests
+
 event_id = 4
 
 
 class ChatPlugin(AbstractChatPlugin):
+    help = """/google [запрос] - выдаёт ссылку на запрос в гугле, а также на первый результат"""
+
     def call(self, event):
         if event[0] != event_id:
             return
@@ -13,4 +17,18 @@ class ChatPlugin(AbstractChatPlugin):
             search_request = search_request.replace('&', '%26')
             search_request = search_request.replace(' ', '+')
             search_url = 'https://www.google.com/search?q=' + search_request
-            self.bot.vkapi.messages.send(message=search_url, peer_id=event[3])
+
+            reply = []
+            reply.append(search_url)
+
+            try:
+                rhead = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:49.0) Gecko/20100101 Firefox/49.0'}
+                req = requests.get(search_url,headers=rhead)
+                rtree = lxml.html.fromstring(req.content)
+                lucky = rtree.xpath('//*/h3[@class="r"]/a')[0]
+                reply.append(lucky.text_content())
+                reply.append(lucky.get("href"))
+            except:
+                pass
+
+            self.bot.vkapi.messages.send(message="\n".join(reply), peer_id=event[3])
