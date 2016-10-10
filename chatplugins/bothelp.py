87150ba84c87a5fc45cb79481af90923c6a09d50
hdk5 <hdk5@users.noreply.github.com>
2016-10-10 13:58:30 +0300

    /help

--- chatplugins/bothelp.py
+++ chatplugins/bothelp.py
@@ -2,25 +2,16 @@ import vk
 
 from bot import AbstractChatPlugin
 
-event_id = 4
-
 
 class ChatPlugin(AbstractChatPlugin):
-    """Должен инициализироваться раньше остальных плагинов чата"""
-    def __init__(self, bot):
-        super(ChatPlugin, self).__init__(bot)
-        self.bot.chat_help = []
-
     def call(self, event):
       if event[0] == event_id:
         if event[6].partition(' ')[0] == '/help' and event[3] < 2000000000:
             respond = []
             respond += ['Инструкция по применению:']
+            for name, plugin in self.bot.chatplugins.items():
+                if hasattr(plugin, 'help'):
+                    respond.append(str(plugin.help))
+            respond += ['Подробнее: http://hdk5.ga/cosine']
 
-            respond += self.bot.chat_help  # Должно инициализироваться плагинами. Но пока так
-
-            try:
-                self.bot.vkapi.messages.send(message='\n'.join(respond), peer_id=event[3])
-            except vk.exceptions.VkAPIError as e:
-                if e.code != 9:
-                    raise
+            self.bot.vkapi.messages.send(message='\n'.join(respond), peer_id=event[3])
