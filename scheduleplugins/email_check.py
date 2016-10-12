075740978d8693607b8e8dfd514a65b0cd369ab2
hdk5 <hdk5@users.noreply.github.com>
2016-10-12 10:23:03 +0300

    С почтой что-то не-то

--- scheduleplugins/email_check.py
+++ scheduleplugins/email_check.py
@@ -36,49 +36,55 @@ class SchedulePlugin(AbstractSchedulePlugin):
 
     def call(self):
 
+        print("Mailcheck called with lastcheck=", self.lastcheck)
+
         # логинимся
         imap = imaplib.IMAP4_SSL("imap.mail.ru")
-        imap.login(self.bot.config['email_check']['user'], self.bot.config['email_check']['password'])
-        # заходим во входящие
-        imap.select()
+        try:  
+            imap.login(self.bot.config['email_check']['user'], self.bot.config['email_check']['password'])
+            # заходим во входящие
+            imap.select()
 
-        status_s, response_s = imap.search(None, 'ALL')
+            status_s, response_s = imap.search(None, 'ALL')
+            checktime = time.time()
 
-        if status_s != 'OK':
-            raise Exception("email_check on 'search all'")
+            if status_s != 'OK':
+                raise Exception("email_check on 'search all'")
 
-        new_msgs = []
+            new_msgs = []
 
 
 
-        for num in reversed(response_s[0].split()):
-            status_f, response_f = imap.fetch(num, '(BODY[HEADER])') 
-            
-            if status_f != 'OK':
-                raise Exception("email_check on 'fetch %s'" % num.decode())
+            for num in reversed(response_s[0].split()):
+                status_f, response_f = imap.fetch(num, '(BODY[HEADER])') 
+                
+                if status_f != 'OK':
+                    raise Exception("email_check on 'fetch %s'" % num.decode())
 
-            raw_header = response_f[0][1].decode()
-            header = mailparser.parsestr(raw_header)
-            header_date = email.utils.mktime_tz(email.utils.parsedate_tz(header["Date"]))
+                raw_header = response_f[0][1].decode()
+                header = mailparser.parsestr(raw_header)
+                header_date = email.utils.mktime_tz(email.utils.parsedate_tz(header["Date"]))
 
-            if header_date < self.lastcheck:
-                break
+                if header_date < self.lastcheck:
+                    break
 
-            header_from = really_decode_header(header["From"])
-            header_subj = really_decode_header(header["Subject"])
+                header_from = really_decode_header(header["From"])
+                header_subj = really_decode_header(header["Subject"])
 
-            new_msgs.append((header_from, header_subj))
+                new_msgs.append((header_from, header_subj))
 
-        if new_msgs:
-            response_v = []
-            response_v.append("На почте %d новых сообщений" % len(new_msgs))
+            if new_msgs:
+                response_v = []
+                response_v.append("На почте %d новых сообщений" % len(new_msgs))
 
-            for msg in new_msgs:
-                response_v.append("От: %s\nТема: %s" % msg)
+                for msg in new_msgs:
+                    response_v.append("От: %s\nТема: %s" % msg)
 
-            response_v.append("https://e.mail.ru/messages/inbox")
+                response_v.append("https://e.mail.ru/messages/inbox")
 
-            self.bot.vkapi.messages.send(message='\n'.join(response_v), peer_id=2000000001, random_id=random.randint(1, 12345678))
+                self.bot.vkapi.messages.send(message='\n'.join(response_v), peer_id=2000000001, random_id=random.randint(1, 12345678))
 
-        self.lastcheck = time.time()
-        imap.close()
+            self.lastcheck = checktime
+
+        finally:
+            imap.close()
