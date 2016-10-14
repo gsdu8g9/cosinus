3f78adff3e9d0f064224193dfb3f2207adc9bd06
hdk5 <hdk5@users.noreply.github.com>
2016-10-14 05:30:01 +0300

    Derpibooru API теперь позволяет получить случайную картинку несколько иначе

--- chatplugins/pone.py
+++ chatplugins/pone.py
@@ -1,43 +1,41 @@
 import io
-import random
+import urllib.parse
 
 import requests
 
 from bot import AbstractChatPlugin
 
-event_id = 4
 
 class ChatPlugin(AbstractChatPlugin):
     help = """/pony - присылает в ответ случайную картинку с поняшкой"""
+
+    booru = "trixiebooru.org"
+    search_string = "score.gt:200,safe,-animated,-svg,-comic"
+
     def call(self, event):
-        if event[0] != event_id:
+        if event[0] != 4:
             return
         if event[6].partition(' ')[0] == '/pony':
-            self.bot.vkapi.messages.setActivity(type='typing',peer_id=event[3])
-            search_req = 'score.gt:200,safe,-animated,-svg,-comic'
-            search_url = 'https://derpibooru.org/search.json'
-            page_req = requests.get("{b}?q={s}".format(b=search_url, s=search_req), timeout=3)
-            page_req.raise_for_status()
-            total = page_req.json()['total']
-            pages = -(-total // 15)
-            page = random.randint(1, pages)
-            req = requests.get("{b}?q={s}&page={p}".format(b=search_url, s=search_req, p=page), timeout=3)
-            req.raise_for_status()
-            random_img = random.choice(req.json()['search'])
-            image_url = 'https:' + random_img['image']
-            image_dbid = random_img['id']
-            image_src = random_img['source_url']
-            image_req = requests.get(image_url)
+            random_id_req = requests.get("https://%s/search.json?q=%s&random_image=1" % (self.booru, self.search_string))
+            random_id_req.raise_for_status()
+            image_id = random_id_req.json()["id"]
+            image_req = requests.get("https://%s/%s.json" % (self.booru, image_id))
             image_req.raise_for_status()
-            image = io.BytesIO(image_req.content)
-            self.bot.vkapi.messages.setActivity(type='typing',peer_id=event[3])
-            upload_server = self.bot.vkapi.photos.getMessagesUploadServer()
-            upload_req = requests.post(upload_server['upload_url'],
-                                       files={'photo': ('image.png', image, image_req.headers['content-type'])})
-            upload_resp = self.bot.vkapi.photos.saveMessagesPhoto(**upload_req.json())
+            image_json = image_req.json()
+            image_url = "https:" + image_json['image']
+            image_data_req = requests.get(image_url)
+            image_data_req.raise_for_status()
+            image_data = io.BytesIO(image_data_req.content)
+            vk_upload_server = self.bot.vkapi.photos.getMessagesUploadServer()
+            vk_upload_req = requests.post(vk_upload_server['upload_url'],
+                                          files={'photo': ('image.png', image_data, image_data_req.headers['content-type'])})
+            vk_upload_resp = self.bot.vkapi.photos.saveMessagesPhoto(**vk_upload_req.json())
 
-            image_vkid = upload_resp[0]['id']
+            image_vkid = vk_upload_resp[0]['id']
 
-            self.bot.vkapi.messages.send(message='http://derpibooru.org/' + str(image_dbid) + '\n' + image_src,
+            vk_response = "https://derpibooru.org/%s\n%s" % (image_id, image_json['source_url'])
+
+
+            self.bot.vkapi.messages.send(message=vk_response,
                                 attachment='photo' + str(self.bot.bot_id) + '_' + str(image_vkid),
                                 peer_id=event[3])
