8c0914010fd1639c7cf97c49041640279c6ee281
hdk5 <hdk5@users.noreply.github.com>
2016-10-15 05:49:42 +0300

    Генерализация email_check Есть мнение, что ini-конфиги было плохой идеей

---
+++ scheduleplugins/email_check/__init__.py
@@ -0,0 +1,92 @@
+import imaplib
+import random
+
+from .config import config
+from bot import AbstractSchedulePlugin
+from email.parser import Parser
+from email.header import decode_header
+import email.utils
+import time
+import logging
+
+mailparser = Parser()
+
+
+def really_decode_header(encoded_header):
+    if type(encoded_header) is not str:
+        raise TypeError("expected str as encoded_header")
+
+    decoded_parts = []
+    for (decoded, charset) in decode_header(encoded_header):
+        if charset is None:
+            if type(decoded) is str:
+                decoded_parts.append(decoded)
+            elif type(decoded) is bytes:
+                decoded_parts.append(decoded.decode())
+        else:
+            decoded_parts.append(decoded.decode(charset))
+
+    return ''.join(decoded_parts)
+
+
+class SchedulePlugin(AbstractSchedulePlugin):
+
+    def __init__(self, bot):
+        super(SchedulePlugin, self).__init__(bot)
+        self.interval = {'minutes': self.bot.config['email_check'].getint('interval')}
+        self.lastcheck = {x: time.time() for x in config.keys()}
+
+    def call(self):
+        # 1: {
+        #     "server": "imap.mail.ru",
+        #     "credentials": ("login", "password")
+        # }
+        for (chat_id, server) in config.items():
+            logging.warning("Mailcheck %d %d" % (chat_id, self.lastcheck[chat_id]))
+            try:
+                imap = imaplib.IMAP4_SSL(server["server"])
+                imap.login(*server['credentials'])
+                # заходим во входящие
+                imap.select()
+
+                status_s, response_s = imap.search(None, 'ALL')
+                checktime = time.time()
+
+                if status_s != 'OK':
+                    raise Exception("email_check on 'search all'")
+
+                new_msgs = []
+
+                for num in reversed(response_s[0].split()):
+                    status_f, response_f = imap.fetch(num, '(BODY[HEADER])')
+
+                    if status_f != 'OK':
+                        raise Exception("email_check on 'fetch %s'" % num.decode())
+
+                    raw_header = response_f[0][1].decode()
+                    header = mailparser.parsestr(raw_header)
+                    header_date = email.utils.mktime_tz(email.utils.parsedate_tz(header["Date"]))
+
+                    if header_date < self.lastcheck[chat_id]:
+                        break
+
+                    header_from = really_decode_header(header["From"])
+                    header_subj = really_decode_header(header["Subject"])
+
+                    new_msgs.append((header_from, header_subj))
+
+                if new_msgs:
+                    response_v = []
+                    response_v.append("На почте %d новых сообщений" % len(new_msgs))
+
+                    for msg in new_msgs:
+                        response_v.append("От: %s\nТема: %s" % msg)
+
+                    response_v.append("https://e.mail.ru/messages/inbox")
+
+                    self.bot.vkapi.messages.send(message='\n'.join(response_v), chat_id=chat_id, random_id=random.randint(1, 12345678))
+
+                self.lastcheck[chat_id] = checktime
+
+            except:
+                logging.exception('email_check')
