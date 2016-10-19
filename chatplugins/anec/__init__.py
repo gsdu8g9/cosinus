af2b7c76bcaad9298fd9064582db342bce913ff5
hdk5 <hdk5@users.noreply.github.com>
2016-10-19 09:08:39 +0300

    Update chatplugins/anec/__init__.py

--- chatplugins/anec/__init__.py
+++ chatplugins/anec/__init__.py
@@ -1,20 +1,19 @@
-import json
-import os
 import random
 
 from bot import AbstractChatPlugin
 
-anec = json.load(open(os.path.join(os.path.dirname(__file__), 'anec.json'), encoding='utf8'))
+from .anecs import anecs
 
-event_id = 4
 
 class ChatPlugin(AbstractChatPlugin):
     help = '''/анекдот - рассказать очень смешной анекдот'''
+
     def call(self, event):
-      if event[0] == event_id:
+        if event[0] != 4:
+            return
         if event[6].partition(' ')[0] == '/анекдот':
             try:
                 n = int(event[6].partition(' ')[2])
             except ValueError:
-                n = random.randint(0, len(anec) - 1)
-            self.bot.vkapi.messages.send(message='Анекдот ' + str(n) + '\n' + anec[n], peer_id=event[3])
+                n = random.randint(0, len(anecs) - 1)
+            self.bot.vkapi.messages.send(message='Анекдот %d\n%s' % (n, anecs[n]), peer_id=event[3])
