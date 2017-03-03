7a09f88e1f56286acbe9e2917e38436582cc53dc
hdk5 <humandk55@gmail.com>
2017-03-03 20:03:52 +0300

    Котики :3

--- chatplugins/pone.py
+++ chatplugins/pone.py
@@ -1,4 +1,5 @@
 import io
+from random import randint
 
 import requests
 
@@ -6,19 +7,16 @@ from bot import AbstractChatPlugin
 
 
 class ChatPlugin(AbstractChatPlugin):
-    help = """/pony - присылает в ответ случайную картинку с поняшкой"""
-
-    booru = "trixiebooru.org"
-    search_string = "score.gt:200,safe,-animated,-svg,-comic"
+    help = """/pony - присылает в ответ случайную картинку с поняшкой, /котик - c котиком"""
 
     def call(self, event):
         if event[0] != 4:
             return
         if event[6].partition(' ')[0] == '/pony':
-            random_id_req = requests.get("https://%s/search.json?q=%s&random_image=1" % (self.booru, self.search_string))
+            random_id_req = requests.get("https://trixiebooru.org/search.json?q=score.gt:200,safe,-animated,-svg,-comic&random_image=1")
             random_id_req.raise_for_status()
             image_id = random_id_req.json()["id"]
-            image_req = requests.get("https://%s/%s.json" % (self.booru, image_id))
+            image_req = requests.get("https://trixiebooru.org/%s.json" % image_id)
             image_req.raise_for_status()
             image_json = image_req.json()
             image_url = "https:" + image_json['image']
@@ -32,6 +30,24 @@ class ChatPlugin(AbstractChatPlugin):
 
             vk_response = "https://derpibooru.org/%s\n%s" % (image_id, image_json['source_url'])
 
+            self.bot.vkapi.messages.send(message=vk_response,
+                                         attachment='photo%d_%d' % (self.bot.bot_id, image_vkid),
+                                         peer_id=event[3])
+
+        if event[6].partition(' ')[0] == '/котик':
+            image_id = randint(1, 3000)
+            image_req = requests.get("https://e926.net/post/index.json?page=%d&tags=cat order:score -meme type:jpg type:png&limit=1" % image_id)
+            image_req.raise_for_status()
+            image_json = image_req.json()[0]
+            image_url = image_json['file_url']
+            image_data_req = requests.get(image_url)
+            image_data_req.raise_for_status()
+            image_data = io.BytesIO(image_data_req.content)
+
+            vk_upload_resp = self.bot.upload_message_image(image_data)
+            image_vkid = vk_upload_resp[0]['id']
+
+            vk_response = "https://e926.net/post/show/%d\n%s" % (image_id, image_json['source'])
             self.bot.vkapi.messages.send(message=vk_response,
                                          attachment='photo%d_%d' % (self.bot.bot_id, image_vkid),
                                          peer_id=event[3])
