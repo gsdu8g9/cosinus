9fa89d0b9df13a0cea1a636192a90b8c86ce9723
hdk5 <humandk55@gmail.com>
2017-03-13 19:45:21 +0300

    Новые экспериментальные котики

--- plugins/pone.py
+++ plugins/pone.py
@@ -1,4 +1,5 @@
 import io
+import os
 from random import randint
 
 import requests
@@ -12,6 +13,29 @@ class Plugin:
     def __init__(self, bot):
         self.bot = bot
         self.bot.add_longpoll_parser("pone", self.call)
+        self.update_ib_sid()
+
+    def update_ib_sid(self):
+        req = requests.get("https://inkbunny.net/api_login.php?"
+                           "username={username}&password={password}".format(username=os.environ.get('INKBUNNY_LOGIN', 'guest'),
+                                                                            password=os.environ.get('INKBUNNY_PASS', '')))
+        resp = req.json()
+        if 'error_code' in resp:
+            raise Exception
+        self.ib_sid = resp['sid']
+
+    def get_ib(self, query):
+        req = requests.get("https://inkbunny.net/api_search.php?sid={sid}&text={query}&"
+                           "count_limit=50000&random=yes&type=1&submissions_per_page=1".format(sid=self.ib_sid, query=query))
+        resp = req.json()
+        if 'error_code' in resp:
+            if resp['error_code'] == 1:
+                self.update_ib_sid()
+                return self.get_ib(query)
+            else:
+                raise Exception
+        return resp['submissions'][0]
+
 
     def call(self, event):
         if event.type != longpoll.VkEventType.MESSAGE_NEW:
@@ -34,18 +58,13 @@ class Plugin:
 
         if t in ('/котик', "/рептилия"):
             if t == '/котик':
-                image_id = randint(1, 1500)
-                image_req = requests.get("https://e926.net/post/index.json?page=%d&tags=cat order:score -meme type:jpg type:png&limit=1" % image_id)
+                resp = self.get_ib('cat')
             if t == '/рептилия':
-                image_id = randint(1, 1500)
-                image_req = requests.get("https://e926.net/post/index.json?page=%d&tags=reptile order:score -meme type:jpg type:png&limit=1" % image_id)
-            image_req.raise_for_status()
-            image_json = image_req.json()[0]
-            image_url = image_json['file_url']
-            image_data_req = requests.get(image_url)
+                resp = self.get_ib('reptile')
+            image_data_req = requests.get(resp['file_url_full'])
             image_data_req.raise_for_status()
             image_data = io.BytesIO(image_data_req.content)
-            vk_response = "https://e926.net/post/show/%d\n%s" % (image_json['id'], image_json['source'])
+            vk_response = "https://inkbunny.net/submissionview.php?id=%s" % (resp['submission_id'])
 
         vk_upload_resp = self.bot.upload_message_photo([image_data])
         image_vkid = vk_upload_resp[0]['id']
