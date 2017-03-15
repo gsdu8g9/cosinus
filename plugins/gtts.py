b3204d3c894665ab363be79b3cac8c54213ac201
hdk5 <humandk55@gmail.com>
2017-03-15 20:54:58 +0300

    Улучшения для гуглтётки

--- plugins/gtts.py
+++ plugins/gtts.py
@@ -1,9 +1,15 @@
-import longpoll
-from gtts import gTTS
 import io
+
+from gtts import gTTS
+from pydub import AudioSegment
+
+import longpoll
 
 
 class Plugin:
+
+    silence3 = AudioSegment.silent(duration=3000)
+
     def __init__(self, bot):
         self.bot = bot
         self.bot.add_longpoll_parser("gtts", self.call)
@@ -16,6 +22,10 @@ class Plugin:
             tts_data = io.BytesIO()
             tts.write_to_fp(tts_data)
             tts_data.seek(0)
+            aseg = AudioSegment.from_mp3(tts_data)
+            tts_data.seek(0)
+            aseg += self.silence3
+            aseg.export(tts_data, format="mp3", tags={"artist": "ГуглТётка", "title": "Сообщение"})
             vk_upload_resp = self.bot.upload_audio(tts_data)
             audio_vkid = vk_upload_resp['id']
             self.bot.api.messages.send(attachment='audio%d_%d' % (self.bot.bot_id, audio_vkid),
